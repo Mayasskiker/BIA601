ee172cdbaad1fc9104a8d2571b813a3c1774cfa2
Mayasskiker <118746333+Mayasskiker@users.noreply.github.com>
2025-11-04 11:50:13 +0300

    Format code in __init__.py for consistency

--- app/__init__.py
+++ app/__init__.py
@@ -14,6 +14,7 @@ app.secret_key = "supersecretkey"
 UPLOAD_FOLDER = os.path.abspath(os.path.join(base_dir, "..", "uploads"))
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
-app.config["MAX_CONTENT_LENGTH"] = 1024 * 1024 * 1024  # 1GB
+app.config["MAX_CONTENT_LENGTH"] = 1024 * 1024 * 1024 
 
 from app import routes
+
