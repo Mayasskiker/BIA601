978bb07cacb0cb47ae55e8b828d73a156ac0278a
Mayasskiker <118746333+Mayasskiker@users.noreply.github.com>
2025-11-04 11:49:08 +0300

    Initialize Flask app with configuration settings

---
+++ app/__init__.py
@@ -0,0 +1,19 @@
+from flask import Flask
+import os
+
+base_dir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(
+    __name__,
+    static_folder=os.path.join(base_dir, "static"),
+    template_folder=os.path.join(base_dir, "templates")
+)
+
+app.secret_key = "supersecretkey"
+
+UPLOAD_FOLDER = os.path.abspath(os.path.join(base_dir, "..", "uploads"))
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+app.config["MAX_CONTENT_LENGTH"] = 1024 * 1024 * 1024  # 1GB
+
+from app import routes
